d322f295084ddafe903d2eb190fb8758dbaaccc3
nextbug <nextbug@ya.ru>
2024-01-05 02:28:14 +0300

    fix

--- internal/mw/auth/cookie.go
+++ internal/mw/auth/cookie.go
@@ -18,11 +18,11 @@ func WithCookieLogin(log *slog.Logger) func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			login, err := GetCookie(log, r)
 			if errors.Is(err, errAuth) {
-				log.Error("Error getting cookie", err.Error())
+				log.Error("Error getting cookie", "package", "auth", "file", "cookie.go", "error", err.Error())
 				http.Error(w, "You are not authenticated", http.StatusUnauthorized)
 				return
 			} else if err != nil {
-				log.Error("Error reading cookie", err)
+				log.Error("Error reading cookie", "package", "auth", "file", "cookie.go", "error", err.Error())
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 				return
 			}
