35f06d2f6ee48bef96d36ad5a70002299c6409ec
nextbug <nextbug@ya.ru>
2024-01-18 18:49:35 +0300

    fix

--- internal/controllers/withdrawals.go
+++ internal/controllers/withdrawals.go
@@ -2,7 +2,11 @@ package controllers
 
 import (
 	"net/http"
+
+	"github.com/nextlag/gomart/internal/mw/auth"
 )
 
 func (c Controller) Withdrawals(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value(auth.LoginKey).(string)
+	w.Write([]byte(user))
 }
