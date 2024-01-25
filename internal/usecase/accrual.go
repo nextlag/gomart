b58852f2d730e620e902c288b3e8112aabda26d0
nextbug <nextbug@ya.ru>
2024-01-25 15:14:43 +0300

    fix

--- internal/usecase/accrual.go
+++ internal/usecase/accrual.go
@@ -120,7 +120,7 @@ func (uc *UseCase) UpdateStatus(ctx context.Context, orderAccrual OrderResponse,
 	_, err := db.NewUpdate().
 		Model(orderModel).
 		Set("status = ?, accrual = ?", orderAccrual.Status, orderAccrual.Accrual).
-		Where(`"number" = ?`, orderAccrual.Order).
+		Where(`"order" = ?`, orderAccrual.Order).
 		Exec(ctx)
 	if err != nil {
 		return err
