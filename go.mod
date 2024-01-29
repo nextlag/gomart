d42ddd4355406a44c3c60e1b776d32c9d401c8c4
nextbug <nextbug@ya.ru>
2024-01-29 19:31:11 +0300

    add register_test

--- go.mod
+++ go.mod
@@ -8,19 +8,24 @@ require (
 	github.com/go-chi/chi/v5 v5.0.11
 	github.com/go-resty/resty/v2 v2.11.0
 	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/golang/mock v1.6.0
 	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.8.3
 	github.com/uptrace/bun v1.1.16
 	github.com/uptrace/bun/dialect/pgdialect v1.1.16
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/stretchr/testify v1.8.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/controllers/controllers_test.go
@@ -0,0 +1,167 @@
+package controllers
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/nextlag/gomart/internal/config"
+	"github.com/nextlag/gomart/internal/controllers/mocks"
+	"github.com/nextlag/gomart/internal/mw/logger"
+	"github.com/nextlag/gomart/internal/usecase"
+)
+
+func controller(t *testing.T) (*Controller, *mocks.MockUseCase, *usecase.UseCase) {
+	t.Helper()
+	log := logger.SetupLogger()
+	var cfg config.HTTPServer
+	mockCtl := gomock.NewController(t)
+	defer mockCtl.Finish()
+
+	repo := mocks.NewMockUseCase(mockCtl)
+	db := usecase.NewMockRepository(mockCtl)
+	uc := usecase.New(db, log, cfg)
+
+	controller := New(repo, log)
+	return controller, repo, uc
+}
+
+func TestRegistrationHandler(t *testing.T) {
+	type want struct {
+		statusCode int
+	}
+
+	tests := []struct {
+		name string
+		want want
+		body string
+	}{
+		{
+			name: "Registration success",
+			want: want{statusCode: http.StatusOK},
+			body: `{"login": "test", "password": "12345"}`,
+		},
+		{
+			name: "Duplicate login",
+			want: want{statusCode: http.StatusConflict},
+			body: `{"login": "duplicate", "password": "12345"}`,
+		},
+		{
+			name: "Internal server error",
+			want: want{statusCode: http.StatusInternalServerError},
+			body: `{"login": "error", "password": "error"}`,
+		},
+		{
+			name: "Empty password",
+			want: want{statusCode: http.StatusOK},
+			body: `{"login": "one"}`,
+		},
+		{
+			name: "Empty login",
+			want: want{statusCode: http.StatusBadRequest},
+			body: `{"password": "12345"}`,
+		},
+		{
+			name: "Invalid request",
+			want: want{statusCode: http.StatusBadRequest},
+			body: `{"log": "test", "pass": "test"}`,
+		},
+		{
+			name: "Empty request",
+			want: want{statusCode: http.StatusBadRequest},
+			body: "",
+		},
+		{
+			name: "Empty JSON request",
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+			body: `{}`,
+		},
+		{
+			name: "Empty",
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// t.Parallel()
+			ctrl, repo, uc := controller(t)
+			repo.EXPECT().Do().Return(uc).Times(2)
+			switch {
+			case tt.name == "Internal server error":
+				repo.EXPECT().DoRegister(context.Background(), gomock.Any(), gomock.Any(), gomock.Any()).Return(errors.New("internal server error")).Times(1)
+			case tt.name == "Duplicate login":
+				// создаем экземпляр pq.Error
+				err := pq.Error{
+					Message: "pq: duplicate key value violates unique constraint \"users_pkey\"",
+					Code:    "23505",
+				}
+				repo.EXPECT().DoRegister(context.Background(), gomock.Any(), gomock.Any(), gomock.Any()).Return(&err).Times(1)
+			default:
+				repo.EXPECT().DoRegister(context.Background(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil).Times(1)
+			}
+			r, err := http.NewRequest(http.MethodPost, "/api/user/register", bytes.NewBufferString(tt.body))
+			w := httptest.NewRecorder()
+			handler := http.HandlerFunc(ctrl.Register)
+			handler(w, r)
+			require.NoError(t, err)
+			assert.Equal(t, w.Code, tt.want.statusCode, "Код ответа не совпадает с ожидаемым")
+		})
+	}
+}
+
+func TestAuthenticationHandler(t *testing.T) {
+	type want struct {
+		statusCode int
+	}
+	tests := []struct {
+		name string
+		want want
+		body string
+	}{
+		{
+			name: "Valid auth",
+			want: want{statusCode: http.StatusOK},
+			body: `{"login": "test", "password": "12345"}`,
+		},
+		{
+			name: "NoValid auth",
+			want: want{statusCode: http.StatusUnauthorized},
+			body: `{"login": "wrong", "password": ""}`,
+		},
+		{
+			name: "Wrong request",
+			want: want{statusCode: http.StatusBadRequest},
+			body: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// t.Parallel()
+			ctrl, repo, uc := controller(t)
+			repo.EXPECT().Do().Return(uc).Times(2)
+			if tt.name == "NoValid auth" {
+				repo.EXPECT().DoAuth(context.Background(), gomock.Any(), gomock.Any(), gomock.Any()).Return(errors.New("unauthorized")).Times(1)
+			}
+			repo.EXPECT().DoAuth(context.Background(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil).Times(1)
+			r, err := http.NewRequest(http.MethodPost, "/api/user/login", bytes.NewBufferString(tt.body))
+			w := httptest.NewRecorder()
+			handler := http.HandlerFunc(ctrl.Authentication)
+			handler(w, r)
+			require.NoError(t, err)
+			assert.Equal(t, w.Code, tt.want.statusCode, "Код ответа не совпадает с ожидаемым")
+		})
+	}
+}
