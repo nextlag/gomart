5a26bb49bda33af5fcf271d166b74d1c698b41d7
nextbug <nextbug@ya.ru>
2024-02-02 12:31:05 +0300

    fix main

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -60,12 +60,12 @@ func main() {
 	r := chi.NewRouter()
 	r.Mount("/", controller.Router(r))
 
-	// go func() {
-	// 	if err = db.Sync(); err != nil {
-	// 		// Обработка ошибок, например, логирование
-	// 		log.Error("error in uc.Sync()", "error", err.Error())
-	// 	}
-	// }()
+	go func() {
+		if err = db.Sync(); err != nil {
+			// Обработка ошибок, например, логирование
+			log.Error("error in uc.Sync()", "error", err.Error())
+		}
+	}()
 
 	// init server
 	srv := setupServer(r)
