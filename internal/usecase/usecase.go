052d85308479f4084d69c309425f3300ac82ed6c
nextbug <nextbug@ya.ru>
2024-02-06 12:55:54 +0300

    fix

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -56,30 +56,24 @@ func (uc *UseCase) DoRegister(ctx context.Context, login, password string, _ *ht
 	return err
 }
 func (uc *UseCase) DoAuth(ctx context.Context, login, password string, _ *http.Request) error {
-	err := uc.repo.Auth(ctx, login, password)
-	return err
+	return uc.repo.Auth(ctx, login, password)
 }
 func (uc *UseCase) DoInsertOrder(ctx context.Context, user string, order string) error {
-	err := uc.repo.InsertOrder(ctx, user, order)
-	return err
+	return uc.repo.InsertOrder(ctx, user, order)
 }
 
 func (uc *UseCase) DoGetOrders(ctx context.Context, user string) ([]byte, error) {
-	orders, err := uc.repo.GetOrders(ctx, user)
-	return orders, err
+	return uc.repo.GetOrders(ctx, user)
 }
 
 func (uc *UseCase) DoGetBalance(ctx context.Context, login string) (float32, float32, error) {
-	b, w, err := uc.repo.GetBalance(ctx, login)
-	return b, w, err
+	return uc.repo.GetBalance(ctx, login)
 }
 
 func (uc *UseCase) DoDebit(ctx context.Context, user, order string, sum float32) error {
-	err := uc.repo.Debit(ctx, user, order, sum)
-	return err
+	return uc.repo.Debit(ctx, user, order, sum)
 }
 
 func (uc *UseCase) DoGetWithdrawals(ctx context.Context, user string) ([]byte, error) {
-	orders, err := uc.repo.GetWithdrawals(ctx, user)
-	return orders, err
+	return uc.repo.GetWithdrawals(ctx, user)
 }
