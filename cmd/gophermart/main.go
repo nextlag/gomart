a28a21d174eb8a6dc797bc2da32951a06c83c00b
nextbug <nextbug@ya.ru>
2024-02-17 13:02:51 +0300

    fix chanel

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 
 	"github.com/go-chi/chi/v5"
@@ -61,21 +60,6 @@ func main() {
 	r := chi.NewRouter()
 	r.Mount("/", controller.Router(r))
 
-	// Инициализация счетчика WaitGroup
-	var wg sync.WaitGroup
-
-	// Увеличение счетчика горутин
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done() // Уменьшение счетчика горутин при завершении работы
-
-		if err = db.Sync(); err != nil {
-			// Обработка ошибок, например, логирование
-			log.Error("error in uc.Sync()", "error", err.Error())
-		}
-	}()
-
 	// init server
 	srv := setupServer(r)
 
@@ -84,19 +68,30 @@ func main() {
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
+	// Канал для сигнализации о завершении сервера HTTP
+	srvDone := make(chan struct{})
+
 	// Запуск HTTP-сервера в горутине
 	go func() {
-		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
-			// Если сервер не стартовал, логируем ошибку
+		defer close(srvDone)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			log.Error("failed to start server", "error main", err.Error())
 			done <- os.Interrupt
+			return
 		}
 	}()
+
 	log.Info("server started")
 
-	<-done // Ожидание сигнала завершения
-	log.Info("server stopped")
+	go func() {
+		defer func() {
+			srvDone <- struct{}{}
+		}()
+		if err := db.Sync(); err != nil {
+			log.Error("error in db.Sync()", "error", err.Error())
+		}
+	}()
 
-	// Ожидание завершения всех горутин
-	wg.Wait()
+	<-done
+	log.Info("server stopped")
 }
